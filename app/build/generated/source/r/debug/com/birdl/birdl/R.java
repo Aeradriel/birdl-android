28a20a50fbd2295c1b919a78278b51b5721f5f1e
Serkan Sahin <serkan.sahin210@gmail.com>
2015-06-11 21:17:44 +0200

    Cleaning the repo to welcome login page process

--- app/build/generated/source/r/debug/com/birdl/birdl/R.java
+++ app/build/generated/source/r/debug/com/birdl/birdl/R.java
@@ -8,32 +8,89 @@
 package com.birdl.birdl;
 
 public final class R {
+    public static final class array {
+        public static final int country_arrays=0x7f050000;
+    }
     public static final class attr {
     }
+    public static final class color {
+        public static final int selected=0x7f060000;
+        public static final int unselected=0x7f060001;
+    }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f070000;
+        public static final int activity_vertical_margin=0x7f070001;
+    }
+    public static final class drawable {
+        public static final int logo_birdl=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int about=0x7f0b000e;
+        public static final int action_bar_message=0x7f0b0005;
+        public static final int action_bar_nottif=0x7f0b0006;
+        public static final int action_bar_settings=0x7f0b0007;
+        public static final int action_bar_user_name=0x7f0b0004;
+        public static final int action_bar_user_picture=0x7f0b0003;
+        public static final int checkBox1=0x7f0b000a;
+        public static final int code=0x7f0b0009;
+        public static final int deconnection=0x7f0b000f;
+        public static final int language=0x7f0b000b;
+        public static final int settings=0x7f0b000d;
+        public static final int spinner=0x7f0b000c;
+        public static final int txt=0x7f0b0008;
+        public static final int txt2=0x7f0b0000;
+        public static final int txt3=0x7f0b0001;
+        public static final int txt4=0x7f0b0002;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int about_activity=0x7f040000;
+        public static final int activity_main=0x7f040001;
+        public static final int custom_action_bar=0x7f040002;
+        public static final int custom_action_bar_about=0x7f040003;
+        public static final int custom_action_bar_mail=0x7f040004;
+        public static final int custom_action_bar_settings=0x7f040005;
+        public static final int mail_activity=0x7f040006;
+        public static final int settings_activity=0x7f040007;
     }
     public static final class menu {
-        public static final int menu_main=0x7f070000;
+        public static final int other_option=0x7f0a0000;
     }
     public static final class mipmap {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_attach=0x7f030000;
+        public static final int ic_action_call=0x7f030001;
+        public static final int ic_action_copy=0x7f030002;
+        public static final int ic_action_cut=0x7f030003;
+        public static final int ic_action_delete=0x7f030004;
+        public static final int ic_action_done=0x7f030005;
+        public static final int ic_action_edit=0x7f030006;
+        public static final int ic_action_locate=0x7f030007;
+        public static final int ic_action_mail=0x7f030008;
+        public static final int ic_action_mail_add=0x7f030009;
+        public static final int ic_action_microphone=0x7f03000a;
+        public static final int ic_action_overflow=0x7f03000b;
+        public static final int ic_action_paste=0x7f03000c;
+        public static final int ic_action_photo=0x7f03000d;
+        public static final int ic_action_refresh=0x7f03000e;
+        public static final int ic_action_search=0x7f03000f;
+        public static final int ic_action_select_all=0x7f030010;
+        public static final int ic_action_send=0x7f030011;
+        public static final int ic_action_share=0x7f030012;
+        public static final int ic_action_star=0x7f030013;
+        public static final int ic_action_user=0x7f030014;
+        public static final int ic_action_user_add=0x7f030015;
+        public static final int ic_action_video=0x7f030016;
+        public static final int ic_launcher=0x7f030017;
     }
     public static final class string {
-        public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int hello_world=0x7f050002;
+        public static final int Privacy_policy=0x7f080000;
+        public static final int Terms_and_agreement_of_service=0x7f080001;
+        public static final int Use_of_BIRDL=0x7f080002;
+        public static final int action_settings=0x7f080003;
+        public static final int country_prompt=0x7f080004;
+        public static final int hello_world=0x7f080005;
+        public static final int policy_and_privacy=0x7f080006;
     }
     public static final class style {
-        /**  Customize your theme here. 
-         */
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f090000;
     }
 }
