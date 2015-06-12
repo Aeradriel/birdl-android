a39aca41c32653c335866cf12511c731c0e5bb55
serkansahin <serkan.sahin210@gmail.com>
2015-06-12 11:35:55 +0200

    Added Login Process - Not Fixed Commit

--- app/build/generated/source/r/debug/com/birdl/birdl/R.java
+++ app/build/generated/source/r/debug/com/birdl/birdl/R.java
@@ -22,38 +22,56 @@ public final class R {
         public static final int activity_vertical_margin=0x7f070001;
     }
     public static final class drawable {
-        public static final int logo_birdl=0x7f020000;
+        public static final int birdl_dev_login_wall=0x7f020000;
+        public static final int birdl_dev_login_wall2=0x7f020001;
+        public static final int logo_birdl=0x7f020002;
+        public static final int text_logo=0x7f020003;
+        public static final int website_wallpaper=0x7f020004;
     }
     public static final class id {
-        public static final int about=0x7f0b000e;
-        public static final int action_bar_message=0x7f0b0005;
-        public static final int action_bar_nottif=0x7f0b0006;
-        public static final int action_bar_settings=0x7f0b0007;
-        public static final int action_bar_user_name=0x7f0b0004;
-        public static final int action_bar_user_picture=0x7f0b0003;
-        public static final int checkBox1=0x7f0b000a;
-        public static final int code=0x7f0b0009;
-        public static final int deconnection=0x7f0b000f;
-        public static final int language=0x7f0b000b;
-        public static final int settings=0x7f0b000d;
-        public static final int spinner=0x7f0b000c;
-        public static final int txt=0x7f0b0008;
+        public static final int about=0x7f0b0018;
+        public static final int action_bar_message=0x7f0b000e;
+        public static final int action_bar_nottif=0x7f0b000f;
+        public static final int action_bar_settings=0x7f0b0010;
+        public static final int action_bar_user_name=0x7f0b000d;
+        public static final int action_bar_user_picture=0x7f0b000c;
+        public static final int action_settings=0x7f0b0016;
+        public static final int checkBox=0x7f0b0008;
+        public static final int checkBox1=0x7f0b0013;
+        public static final int code=0x7f0b0012;
+        public static final int connect_button=0x7f0b0007;
+        public static final int deconnection=0x7f0b0019;
+        public static final int imageView=0x7f0b0006;
+        public static final int language=0x7f0b0014;
+        public static final int mainview=0x7f0b0003;
+        public static final int settings=0x7f0b0017;
+        public static final int spinner=0x7f0b0015;
+        public static final int textView2=0x7f0b0009;
+        public static final int textView3=0x7f0b000a;
+        public static final int txt=0x7f0b0011;
         public static final int txt2=0x7f0b0000;
         public static final int txt3=0x7f0b0001;
         public static final int txt4=0x7f0b0002;
+        public static final int user_email=0x7f0b0005;
+        public static final int user_pwd=0x7f0b0004;
+        public static final int welcome_fr=0x7f0b000b;
     }
     public static final class layout {
         public static final int about_activity=0x7f040000;
-        public static final int activity_main=0x7f040001;
-        public static final int custom_action_bar=0x7f040002;
-        public static final int custom_action_bar_about=0x7f040003;
-        public static final int custom_action_bar_mail=0x7f040004;
-        public static final int custom_action_bar_settings=0x7f040005;
-        public static final int mail_activity=0x7f040006;
-        public static final int settings_activity=0x7f040007;
+        public static final int activity_login=0x7f040001;
+        public static final int activity_main=0x7f040002;
+        public static final int activity_user=0x7f040003;
+        public static final int custom_action_bar=0x7f040004;
+        public static final int custom_action_bar_about=0x7f040005;
+        public static final int custom_action_bar_mail=0x7f040006;
+        public static final int custom_action_bar_settings=0x7f040007;
+        public static final int mail_activity=0x7f040008;
+        public static final int settings_activity=0x7f040009;
     }
     public static final class menu {
-        public static final int other_option=0x7f0a0000;
+        public static final int menu_login=0x7f0a0000;
+        public static final int menu_user=0x7f0a0001;
+        public static final int other_option=0x7f0a0002;
     }
     public static final class mipmap {
         public static final int ic_action_attach=0x7f030000;
@@ -86,9 +104,24 @@ public final class R {
         public static final int Terms_and_agreement_of_service=0x7f080001;
         public static final int Use_of_BIRDL=0x7f080002;
         public static final int action_settings=0x7f080003;
-        public static final int country_prompt=0x7f080004;
-        public static final int hello_world=0x7f080005;
-        public static final int policy_and_privacy=0x7f080006;
+        public static final int app_name=0x7f080004;
+        public static final int birdl_logo_bird=0x7f080005;
+        public static final int birdl_logo_string=0x7f080006;
+        public static final int connect_fr=0x7f080007;
+        public static final int country_prompt=0x7f080008;
+        public static final int email=0x7f080009;
+        public static final int email_in=0x7f08000a;
+        public static final int hello_world=0x7f08000b;
+        public static final int identify_fr=0x7f08000c;
+        public static final int logout_fr=0x7f08000d;
+        public static final int password_fr=0x7f08000e;
+        public static final int password_in=0x7f08000f;
+        public static final int policy_and_privacy=0x7f080010;
+        public static final int save_text_fr=0x7f080011;
+        public static final int savebox=0x7f080012;
+        public static final int savebox_in=0x7f080013;
+        public static final int title_activity_user=0x7f080014;
+        public static final int welcome_fr=0x7f080015;
     }
     public static final class style {
         public static final int AppTheme=0x7f090000;
---
+++ app/src/main/java/com/birdl/birdl/Login.java
@@ -0,0 +1,72 @@
+package com.birdl.birdl;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+
+public class Login extends Activity {
+    private static EditText username;
+    private static EditText password;
+    private static Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        LoginButton();
+    }
+
+    public void LoginButton() {
+        username = (EditText)findViewById(R.id.user_email);
+        password = (EditText)findViewById(R.id.user_pwd);
+        login = (Button)findViewById(R.id.connect_button);
+
+        login.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (username.getText().toString().equals("serkan.sahin210@gmail.com") &&
+                                password.getText().toString().equals("test")){
+                            Toast.makeText(Login.this, "Successfully logged in :)!",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent("android.intent.action.MAIN");
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(Login.this, "Wrong Username/Password :(",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
