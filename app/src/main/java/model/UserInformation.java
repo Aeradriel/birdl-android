7e28bea9538dd3e87ff1b311a29a7068d53715ae
christophe <christophe.crocheton@gmail.com>
2015-09-17 15:12:27 +0200

    Access token class maj

---
+++ app/src/main/java/model/UserInformation.java
@@ -0,0 +1,65 @@
+package model;
+
+import activity.MainActivity;
+
+/**
+ * Created by Christophe on 15/09/2015.
+ */
+public class UserInformation{
+
+    int id;
+    String email;
+    String first_name;
+    String last_name;
+    String birthdate;
+    int gender;
+    //String country;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+}
---
+++ app/src/main/java/model/UserInformationStatic.java
@@ -0,0 +1,73 @@
+package model;
+
+/**
+ * Created by Christophe on 15/09/2015.
+ */
+public class UserInformationStatic {
+
+    public static int id;
+    public static String first_name;
+    public static String last_name;
+    public static String birthdate;
+    public static int gender;
+    public static String email;
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        UserInformationStatic.id = id;
+    }
+
+    public static String getEmail() {
+        return email;
+    }
+
+    public static void setEmail(String email) {
+        UserInformationStatic.email = email;
+    }
+
+    public static String getLast_name() {
+        return last_name;
+    }
+
+    public static void setLast_name(String last_name) {
+        UserInformationStatic.last_name = last_name;
+    }
+
+    public static String getBirthdate() {
+        return birthdate;
+    }
+
+    public static void setBirthdate(String birthdate) {
+        UserInformationStatic.birthdate = birthdate;
+    }
+
+    public static int getGender() {
+        return gender;
+    }
+
+    public static void setGender(int gender) {
+        UserInformationStatic.gender = gender;
+    }
+
+    public static String getFirst_name() {
+        return first_name;
+
+    }
+
+    public static void setFirst_name(String first_name) {
+        UserInformationStatic.first_name = first_name;
+    }
+
+    public UserInformationStatic(int id, String email, String first_name, String last_name, String birthdate, int gender)
+    {
+        UserInformationStatic.id = id;
+        UserInformationStatic.email = email;
+        UserInformationStatic.first_name = first_name;
+        UserInformationStatic.last_name = last_name;
+        UserInformationStatic.birthdate = birthdate;
+        UserInformationStatic.gender = gender;
+    }
+}
