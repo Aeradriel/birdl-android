96ba3b48f82d3fd63b03e51c5eae183e9dfd5c30
serkansahin <serkan.sahin210@gmail.com>
2015-12-12 16:27:56 +0100

    create message backend done

--- app/src/main/java/com/birdl/activity/NewMessageActivity.java
+++ app/src/main/java/com/birdl/activity/NewMessageActivity.java
@@ -6,23 +6,75 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class NewMessageActivity extends Fragment{
-    public NewMessageActivity() {
-    }
+import configBirdl.BirdlConfigNetwork;
+import configBirdl.RestInterface;
+import configBirdl.UserInformation;
+import configBirdl.UserInformationStatic;
+import interfaceRetrofit.RestMessageInterface;
+import interfaceRetrofit.RestUserInterface;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+
+public class NewMessageActivity extends Fragment {
+    private BirdlConfigNetwork msgNetwork;
+    private RestMessageInterface restCaller;
+    private EditText receiver_id;
+    private String sender_id;
+    private EditText content_msg;
+    private Button send;
+    private UserInformationStatic userinfo;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        msgNetwork = new BirdlConfigNetwork();
+        sendMethod();
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_new_message, container, false);
-
+        send = (Button) rootView.findViewById(R.id.send_but);
+        receiver_id = (EditText) rootView.findViewById(R.id.receiver_id);
+        content_msg = (EditText) rootView.findViewById(R.id.content_msg);
+        sender_id = String.valueOf(UserInformationStatic.getId());
         return rootView;
+
     }
+
+    public void sendMethod() {
+        send.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Thread start = new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                restCaller = msgNetwork.getRestAdapter().create(RestMessageInterface.class);
+                                restCaller.createMessage(sender_id, receiver_id.toString(), content_msg.toString(), new Callback<Response>() {
+                                    @Override
+                                    public void success(Response response, Response response2) {
+                                        Toast.makeText(getActivity(), "Message envoyé", Toast.LENGTH_LONG).show();
+                                    }
+
+                                    @Override
+                                    public void failure(RetrofitError error) {
+                                        Toast.makeText(getActivity(), "Erreur", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            }
+                        });
+                        start.start();
+                    }
+                });
+    }
+
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
